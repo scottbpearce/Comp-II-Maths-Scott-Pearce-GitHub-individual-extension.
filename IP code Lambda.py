3929b6dbc6d71e78e56990e465b5083f46b729b1
scottbpearce <scottpearce50@gmail.com>
2026-01-02 11:10:39 +0000

    Add files via upload

---
+++ IP code Lambda.py
@@ -0,0 +1,13 @@
+def Rgrad2(p):
+    return 8*p - 12*p**2 + 4*p**3
+
+print( Rgrad2(0.381966) )
+
+
+
+def Rgrad3(p):
+    return 32*p - 144*p**2 + 272*p**3 - 280*p**4 + 168*p**5 - 56*p**6 + 8*p**7
+
+
+print( Rgrad3(0.079432) )
+
